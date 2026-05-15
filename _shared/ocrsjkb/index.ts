707438be5c446635fbb73b14c8a158da4a49bb81
mosses1207 <mosses161192@gmail.com>
2026-05-16 00:00:13 +0700

    Create index.ts

---
+++ _shared/ocrsjkb/index.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { prosesGemini } from "../_shared/geminiRole.ts";
+serve(async (req) => {
+  const { base64Images } = await req.json();
+  const result = await prosesGemini(base64Images);
+  return new Response(JSON.stringify(result), {
+    headers: { 'Content-Type': 'application/json' }
+  });
+});
