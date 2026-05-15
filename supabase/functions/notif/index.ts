d145f6a073c7148d9fde1a6a6ecec9a0c78e4a21
mosses1207 <mosses161192@gmail.com>
2026-05-16 00:33:53 +0700

    Update index.ts

--- supabase/functions/notif/index.ts
+++ supabase/functions/notif/index.ts
@@ -1,4 +1,3 @@
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 import CryptoJS from "https://esm.sh/crypto-js@4.1.1"
