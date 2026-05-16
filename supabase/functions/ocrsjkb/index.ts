db6bda60d314082aa5942286bcaa0620092fd8e0
mosses1207 <mosses161192@gmail.com>
2026-05-16 21:04:39 +0700

    Update index.ts

--- supabase/functions/ocrsjkb/index.ts
+++ supabase/functions/ocrsjkb/index.ts
@@ -1,6 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { prosesGemini } from "../_shared/geminiRole.ts";
-
 serve(async (req) => {
   const { base64Images } = await req.json();
   const result = await prosesGemini(base64Images);
